d29665357cc6a5f39f1c5d4ed7dbc43d4130d995
Winston Calhoun <superdevant@gmail.com>
2022-09-05 11:59:56 -0500

    add content

--- objects.js
+++ objects.js
@@ -5,27 +5,28 @@
 // the desired result. To check your work, run this
 // file in your Terminal.
 
-foods = {
-  apples: 23, 
-  grapes: 507, 
+var foods = {
+  apples: 23,
+  grapes: 507,
   eggs: 48
 }
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
-// YOUR CODE HERE
+Object.keys(foods);
 
-// Write code that prints all of the 'values' of the foods variable 
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
-// YOUR CODE HERE
+Object.values(food);
 
-// Write code that prints the value of the second food of the foods variable 
+// Write code that prints the value of the second food of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(foods.grapes);
 
-// Write code that adds a food to the foods object. 
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
-// YOUR CODE HERE
+foods.cherries = 56
+console.log(food.cherries);
 
 
 #-------------------
@@ -39,16 +40,24 @@ foods = {
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+var email =  {
+  recipiant: "Joe",
+  sender: "Winston",
+  subject: "Wassup",
+  date: 5,
+  sent: true
+}
+
 // Write code that logs your email object to the terminal.
+console.log(email);
 
-
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
-// YOUR CODE HERE
+Object.keys(email);
 
-// Write code that logs all of the 'values' of the email object 
+// Write code that logs all of the 'values' of the email object
 // you created above:
-// YOUR CODE HERE
+Object.values(email);
 
 
 #-------------------
@@ -69,14 +78,14 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 // that relate to each individual Instagram post:
 
 
-posts = [
+var posts = [
   {
     imageSrc: "./images/beach.png",
     caption: "At the beach with my besties",
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +99,35 @@ console.log(posts);
 console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
-// key-value pairs you used in your email Object above.
-// Then, print the email Array to the Terminal.
+// YOU DO:
+var email = [
+  {
+    recipiant: "Joe",
+    sender: "Winston",
+    subject: "Wassup",
+    date: 5,
+    sent: true
+  },
+  { recipiant: "Brad",
+    sender: "Winston",
+    subject: "Bored",
+    date: 4,
+    sent:true
+  },
+  { recipiant: "Ryan",
+    sender: "Winston",
+    subject: "Dumb",
+    date: 7,
+    sent: false
+  }
+];
+
+console.log(email);
+console.log(email[0]);
+// the 0 in brackets is the postion.(Joe, Brad, Ryan) in recipiant.
--- selfEval.md
+++ selfEval.md
@@ -1,5 +1,11 @@
 # Self Evaluation
 
 - What did you struggle with in this section?
+// I struggled with getting a handle on the different of when you console.log and when its object.
+
 - How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes?
+// Im finding it more helpful to write notes in my notebook and also writing notes in
+the lessons on atom themselves so i can go back to the lesson and see what I did.
+
 - What are you noticing that you lean into - Googling, or trying to solve a problem in code on your own first?
+// I lean into trying myself.  Googling is like the 4th attempt as they ling and ways other people code are either above me or it is something we havent learned yet.
